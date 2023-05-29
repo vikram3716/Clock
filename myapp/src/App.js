93b7714838e9f638414cf10fb39ba1a635f85217
vikram3716 <vikram99301@gmail.com>
2023-05-29 18:15:01 +0530

    made real time clock

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,7 +4,7 @@ import Clock from './Clock';
 function App() {
   return (
     <div style={{backgroundColor:"",height:"100vh",marginTop:"0"}}>
-    <h1 style={{padding:"0" ,textAlign:"center",fontSize:"50px",color:"white" ,fontStyle:"italic"}}>Clock</h1>
+    <h1 style={{padding:"0" ,textAlign:"center",fontSize:"50px",color:"rgb(237,69,101)" ,fontStyle:"italic",fontFamily:"inherit"}}>Real Time Clock</h1>
     <div className="App">
   <Clock/>
     </div>
