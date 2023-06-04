5e6d77b5636b46fcd6909294abf301e66b2877c9
vikram3716 <vikram99301@gmail.com>
2023-06-04 22:23:06 +0530

    finish

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,19 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import Clock from './Clock';
-import FormPropsTextFields from './Mi'
-import Chakra from './Chakra';
+import logo from "./logo.svg";
+import "./App.css";
+import Clock from "./Clock";
+import FormPropsTextFields from "./Mi";
+import Chakra from "./Chakra";
 function App() {
   return (
-    <div style={{backgroundColor:"",height:"100vh",marginTop:"0"}}>
-    <h1 style={{padding:"0" ,textAlign:"center",fontSize:"50px",color:"rgb(237,69,101)" ,fontStyle:"italic",fontFamily:"inherit"}}>Real Time Clock</h1>
-    <div className="App">
-  <Clock/>
-  <br/>
-  
-  <SignIn />
-  <FormPropsTextFields />
-    </div>
+    <div style={{ backgroundColor: "", height: "100vh", marginTop: "0" }}>
+      <h1
+        style={{
+          padding: "0",
+          textAlign: "center",
+          fontSize: "50px",
+          color: "rgb(237,69,101)",
+          fontStyle: "italic",
+          fontFamily: "inherit",
+        }}
+      >
+        Real Time Clock
+      </h1>
+      <div className="App">
+        <Clock />
+      </div>
     </div>
   );
 }
